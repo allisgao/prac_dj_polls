ac70bd69af25b4f409d5d658bdeef6d6bd08ef00
gaojingzhen <gaojingzhen@idstaff.com>
2018-08-10 00:23:27 +0800

    use render to improve views in  templates

--- polls/views.py
+++ polls/views.py
@@ -7,9 +7,10 @@ from .models import Question
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
