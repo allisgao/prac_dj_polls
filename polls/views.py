3bddebe870866353224a5f3a4d318270ec6ede1a
gaojingzhen <gaojingzhen@idstaff.com>
2018-08-12 09:17:33 +0800

    modify vote function

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 # Create your views here.
 
 
@@ -11,6 +12,7 @@ def index(request):
     # return HttpResponse(output)
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     """
@@ -33,4 +35,23 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    """
+
+    :param request:
+    :param question_id:
+    :return:
+    """
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 防止硬编码
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
