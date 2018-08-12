ec4bcce8231b23eacff02b80a2064d2fde8ef202
gaojingzhen <gaojingzhen@idstaff.com>
2018-08-12 17:39:37 +0800

    improve urls and views

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
 
-
+'''
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,8 +13,17 @@ def index(request):
     # return HttpResponse(output)
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+'''
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
+'''
 def detail(request, question_id):
     """
     try:
@@ -27,27 +37,26 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
     # return HttpResponse("You are looking at question %s." % question_id)
+'''
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
+"""
 def results(request, question_id):
-    """
-
-    :param request:
-    :param question_id:
-    :return:
-    """
     # response = "You're looking at the results of question %s."
     # return HttpResponse(response % question_id)
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+"""
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def vote(request, question_id):
-    """
-
-    :param request:
-    :param question_id:
-    :return:
-    """
     # return HttpResponse("You're voting on question %s." % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -62,3 +71,8 @@ def vote(request, question_id):
         selected_choice.save()
         # 防止硬编码
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+"""
+class VoteView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+"""
