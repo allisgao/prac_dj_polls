b8ad121f035ae20e585135286d56c22cac45fdd6
gaojingzhen <gaojingzhen@idstaff.com>
2018-08-21 21:47:52 +0800

    add a test case

--- polls/test.py
+++ polls/test.py
@@ -36,7 +36,10 @@ class QuestionIndexViewTest(TestCase):
         """
         create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
-        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question>'])
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
 
     def test_future_question(self):
         """
@@ -100,3 +103,24 @@ class QuestionModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future returns a 404 not found.
+        :return:
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_quesiton(self):
+        """
+        The detail view of a question with a pub_date in the past displays the question's text.
+        :return:
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
