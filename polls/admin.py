447e899d2762d679ee94bf77352edcb568e739e2
gaojingzhen <gaojingzhen@idstaff.com>
2018-08-23 22:52:53 +0800

    optimize customize admin-page's form.

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,18 @@ from .models import Question
 
 # admin.site.register(Question)
 
+"""
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
+"""
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
 
 admin.site.register(Question, QuestionAdmin)
